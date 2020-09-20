209f52b48bccc3863f45a1114eb25b58268c697c
Nidhi H <68785516+nidhiii13@users.noreply.github.com>
2020-09-20 13:14:59 +0530

    Update script.js

--- module-4/script.js
+++ module-4/script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["Yami", "John", "Jessi", "Jason", "Paul", "Frank", "Paula", "Laurence", "Jim"];
+    var names = ["Yami", "John", "Jessi", "Jason", "Peter", "Frank", "Paula", "Laurence", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
