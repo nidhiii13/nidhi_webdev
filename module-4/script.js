ea9db87f209be0b4d65d6c078fb43afa5421e9a2
Nidhi H <68785516+nidhiii13@users.noreply.github.com>
2020-09-20 13:07:11 +0530

    Create script.js

---
+++ module-4/script.js
@@ -0,0 +1,11 @@
+(function() {
+    var names = ["Yami", "John", "Jessi", "Jason", "Paul", "Frank", "Paula", "Laurence", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
